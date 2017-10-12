97e31ec69bac50782c41c8bd63a5b88c27a14a12
Shane Zackery <shane@entelo.com>
2017-10-12 08:32:55 -0700

    handle invalid options

--- server.rb
+++ server.rb
@@ -59,36 +59,64 @@ class ChatServer
     @new_socket.write "You're connected to Shane's Chat App Server\n"
     @new_socket.write "Please enter a username\n"
     @new_socket.username = @new_socket.gets.chomp
-    @new_socket.write "Hello, #{@new_socket.username}. Welcome!\n"
+    @new_socket.write "Welcome, #{@new_socket.username}!\n"
+    chat_options
+  end
+
+  def chat_options
     @new_socket.write "Would you like to [1] create a new chat room or [2] join an existing one?\n"
     str = @new_socket.gets.chomp.to_s.downcase
 
     if str == '1'
-      @new_socket.write "Great, what would you like to name your new chat room?\n"
-      @new_chat_room = ChatRoom.new(@new_socket.gets.chomp)
-      @new_socket.current_room = @new_chat_room.name
       create_new_chatroom
-      @chat_members = @new_chat_room.members << @new_socket
-      @new_socket.write "Chat room #{@new_chat_room.name} has been created. You are now in this room.\n"
-
+      announce_new_user
     elsif str == '2'
-      @new_socket.write "Here is the list of rooms: #{@list_chat_rooms}"
-      @new_socket.write "which would you like to enter?\n"
-      @new_socket.current_room = @new_socket.gets.chomp.downcase
-      @new_socket.write "Great, we're connecting you to the #{@new_socket.current_room} chat room\n"
-      @new_socket.write "You are now in the #{@room_choice} chat room\n"
+      join_existing_chatroom
+      announce_new_user
+    elsif str == 'options'
+      chat_options
     else
       @new_socket.write "Invalid choice. Please enter '1' to create a new room or '2' to join an existing room'\n"
+      chat_options
     end
+  end
+
+  def create_new_chatroom
+    @new_socket.write "What would you like to name your new chat room?\n"
+    @new_chat_room = ChatRoom.new(@new_socket.gets.chomp)
+    @new_socket.current_room = @new_chat_room.name
+    @chat_rooms << @new_chat_room.name
+    @chat_members = @new_chat_room.members << @new_socket.username
+    @new_socket.write "Chat room #{@new_chat_room.name} has been created. You are now in this room.\n"
+  end
+
+  def join_existing_chatroom
+    if @chat_rooms.length != 0
+      @new_socket.write "Here is the list of rooms: #{list_chat_rooms()}\n Which would you like to enter?\n"
+      @new_socket.current_room = @new_socket.gets.chomp.downcase
+      if @chat_rooms.include? @new_socket.current_room
+        @new_socket.write "Great, we're connecting you to the #{@new_socket.current_room} chat room\n"
+        @new_socket.write "You are now in the #{@new_socket.current_room} chat room.\n"
+      else
+        @new_socket.write "That room doesn't exist.\n"
+        chat_options
+      end
+    else
+      @new_socket.write "There are currently no active rooms. Please create one!\n"
+      create_new_chatroom
+    end
+  end
+
+  def announce_new_user
     str = "#{@new_socket.username} joined room #{@new_socket.current_room}\n"
     broadcast_string(str, @new_socket)
   end
 
-  def create_new_chatroom
-    @chat_rooms << @new_chat_room.name
+  def list_chat_rooms
+    @chat_rooms.each { |room| puts room }
   end
 
-  def list_chat_rooms
-    @chat_rooms.each { |room| puts "#{room}"}
+  def list_chat_members
+    @chat_members.each {|member| puts member}
   end
 end
